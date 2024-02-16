4ba47abc4c0336996599f3c21422fef0de2ed999
Ben Smith <smithbennettlarue@gmail.com>
2024-02-16 12:24:23 -0500

    Updated chat interface.

--- client/src/components/messageBoxes/UserMessage.js
+++ client/src/components/messageBoxes/UserMessage.js
@@ -2,17 +2,28 @@ import React from "react";
 
 const UserMessage = (props) => {
     return (
-        <div className="flex p-7 bg-cream shadow-md border-y-2 border-slate-200 justify-center">
-            <div className="flex justify-center w-full">
-                <div className="h-12 w-11 flex-shrink-0 mr-5 mt-1">
-                    <img
-                        src="https://i.imgur.com/prghu9r.png"
-                        className="drop-shadow-md w-12 h-12 object-contain"
-                        alt="User"
-                    />
+        <div className="py-5 bg-cream border-y-2 border-slate-200">
+            <div className="flex lg:justify-center md:justify-center w-full h-full px-3 mb-3">
+                <div className="w-2/3">
+                    <div className="h-30 w-30 flex-shrink-0 mr-2 mt-1 flex">
+                        <div className=" rounded-full bg-blue-300 p-1.5">
+                            <img
+                                src="https://i.imgur.com/prghu9r.png"
+                                className="object-contain overflow-clip"
+                                style={{
+                                    height: "23px",
+                                }}
+                                alt="User"
+                            />
+                        </div>
+                        <div className="ml-2 flex items-center font-semibold text-md">
+                            You
+                        </div>
+                    </div>
                 </div>
-
-                <div className="w-2/3 whitespace-normal break-words shadow-lg p-4 border border-slate-200 rounded-xl">
+            </div>
+            <div className="flex lg:justify-center md:justify-center w-full h-full px-3">
+                <div className="lg:w-2/3 md:w-2/3 whitespace-normal break-words p-2 my-1">
                     {props.content}
                 </div>
             </div>
